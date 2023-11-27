b6a79b8df48c05a421b4a8e9f2b549eb0473368b
779 <liz.mishina20@gmail.com>
2023-11-27 23:48:44 +0300

    2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <string>
+#include <Windows.h>
+#include <conio.h>
 using namespace std;
+#define UP 72
+#define DOWN 80
+#define ENTER 13
+HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+void goToXY(short xx, short yy)
+{
+	SetConsoleCursorPosition(hStdOut, { xx,yy });
+}
+void consoleCursorVisible(bool show, short size)
+{
+	CONSOLE_CURSOR_INFO structCursorInfo;
+	GetConsoleCursorInfo(hStdOut, &structCursorInfo);
+	structCursorInfo.bVisible = show;
+	structCursorInfo.dwSize = size;
+	SetConsoleCursorInfo(hStdOut, &structCursorInfo);
+}
 
 class Price
 {
@@ -14,6 +32,7 @@ public:
 	Price(int a)
 	{
 		string text;
+		cout << "First object" << endl << "product: ";
 		getline(cin, text);
 		Setter_product(text);
 		cout << "shop: ";
@@ -80,16 +99,14 @@ public:
 		Price buffer(*this);
 		Setter_size(Getter_size() + 1);
 		array = new Price * [Getter_size()];
-		cout << "First object" << endl << "product: ";
+		
 		
 		for (int i = 0; i < Getter_size()-1; i++)
 		{
 			array[i] = buffer.array[i];
 		}
 		array[Getter_size() - 1] = new Price(1);
-
 		return *this;
-
 	}
 	Price operator -(int number)
 	{
@@ -108,52 +125,141 @@ public:
 		return *this;
 
 	}
+	
+	//void Swap(string* xp, string* yp)
+	//{
+	//	string temp = *xp;
+	//	*xp = *yp;
+	//	*yp = temp;
+	//}
+	//int Partition(string arr[], int low, int high) {
 
+	//	string pivot = arr[high];
+	//	int i = (low - 1);
+
+	//	for (int j = low; j <= high - 1; j++) {
+
+	//		if (arr[j] < pivot) {
+	//			i++;
+	//			Swap(&arr[i], &arr[j]);
+	//		}
+	//	}
+	//	Swap(&arr[i + 1], &arr[high]);
+	//	return (i + 1);
+	//}
+	//void QuickSort(string arr[], int low, int high) {
+	//	if (low < high) {
+	//		int pi = Partition(arr, low, high);
+	//		QuickSort(arr, low, pi - 1);
+	//		QuickSort(arr, pi + 1, high);
+	//	}
+	//}
 };
 int main()
 {
-	string text;
-	Price object;
-	getline(cin, text);
 
-	if (stoi(text) == 1)
-	{
-		object + 1;
-	}
-	getline(cin, text);
-	if (stoi(text) == 1)
-	{
-		object + 1;
-	}
-	getline(cin, text);
-	if (stoi(text) == 1)
-	{
-		object + 1;
-	}
 
-	getline(cin, text);
-	if (stoi(text) == 2)
-	{
-		object.Show_all();
-	}
+		SetConsoleTitle(L"menu");
+		consoleCursorVisible(false, 100);
+		string menu[] = { "1. ADD", "2. DELETE", "3. SHOW ALL", "4. SHOW PRODUCTS FROM A SPECIFIC STORE", "5. EXIT"};
+		int activeMenu = 0;
+		char ch;
+		float x = 0;
+		string text;
+		Price object;
 
-	getline(cin, text);
-	if (stoi(text) == 3)
-	{
-		object.Show_products();
-	}
-	//getline(cin, text);
-	//if (stoi(text) == 3)
-	//{
-	//	cout << "VV num" << endl;
-	//	getline(cin, text);
-	//	object - (stoi(text));
-	//}
-	//getline(cin, text);
-	//if (stoi(text) == 2)
-	//{
-	//	object.Show_all();
-	//}
+		int kolvo = 0;
 
-	return 0;
-}
+		while (1)
+		{
+			int xx = 55, yy = 0;
+			goToXY(xx, yy);
+			for (int i = 0; i < size(menu); i++)
+			{
+				if (i == activeMenu)
+					SetConsoleTextAttribute(hStdOut, FOREGROUND_BLUE |
+						FOREGROUND_INTENSITY);
+				else
+					SetConsoleTextAttribute(hStdOut, FOREGROUND_BLUE);
+				goToXY(xx, yy++);
+				cout << menu[i] << endl;
+			}
+			ch = _getch();
+			if (ch == -32)
+				ch = _getch();
+			switch (ch)
+			{
+			case UP:
+				if (activeMenu > 0)
+					--activeMenu;
+				break;
+			case DOWN:
+				if (activeMenu < size(menu) - 1)
+					++activeMenu;
+				break;
+			case ENTER:
+				switch (activeMenu)
+				{
+				case 0:
+					system("cls");
+					
+						cout << endl << endl;
+						object + 1;
+						kolvo++;
+					
+					break;
+				case 1:
+					system("cls");
+					if (kolvo != 0)
+					{
+						object.Show_all();
+						cout << "CHOOSE: ";
+						//cin >> k;
+						getline(cin, text);
+						if (stoi(text) > 0 && stoi(text) <= kolvo)
+						{
+							object - stoi(text);
+							kolvo--;
+						}
+						else
+						{
+							system("cls");
+							cout << "ERROR!" << endl;
+						}
+					}
+					else
+					{
+						system("cls");
+						cout << "NO OBJECTS!" << endl;
+					}
+					break;
+				case 2:
+					system("cls");
+					if (kolvo != 0)
+						object.Show_all();
+					else
+						cout << "EMPTY!" << endl;
+					break;
+				case 3:
+					system("cls");
+					if (kolvo != 0)
+						object.Show_products();
+					else
+						cout << "EMPTY!" << endl;
+					break;
+
+
+				case 4:
+					system("cls");
+					exit(0);
+					break;
+				
+				default:
+					break;
+				}
+			default:
+				break;
+			}
+		}
+
+	}
